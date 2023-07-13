644536d1bc0a0831ef879000f92ee83e49683d23
edwardedo603 <edwardedo603@gmail.com>
2023-07-13 19:43:47 +0700

    fix: env file

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    MONGO_URI:
-      "mongodb+srv://edward:dwaynEDO13@cluster0.5qi3v.mongodb.net/TakutNgomongApp?retryWrites=true&w=majority",
-  },
 };
 
 module.exports = nextConfig;
